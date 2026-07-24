9f74f3126f7fbb3ac6ec9a1922df432c155686aa
aakash-31 <139964337+aakash-31@users.noreply.github.com>
2026-07-24 17:22:28 +0530

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.cpp
@@ -0,0 +1,42 @@
+class Solution {
+   public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] =='O')
+                dfs(board,  i, 0); 
+            if (board[i][n-1] =='O') 
+                dfs(board,  i, n-1);
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O')
+                dfs(board, 0, j); 
+            if (board[m-1][j] == 'O') 
+                dfs(board, m-1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    // dfs(board, visited, i, j, n, m);
+                        board[i][j]='X';
+
+                }
+                else if(board[i][j] == 't')            
+                    board[i][j]='O';
+                }
+        }
+    }
+    void dfs(vector<vector<char>>&board,int r,int c){
+        if(r<0 || r>=board.size() || c<0 || c>=board[0].size() || board[r][c]!='O')
+            return;
+        // visited[r][c]=true;
+        board[r][c]='t';
+        dfs(board,r+1,c);
+        dfs(board,r-1,c);
+        dfs(board,r,c+1);
+        dfs(board,r,c-1);
+    }
+};
